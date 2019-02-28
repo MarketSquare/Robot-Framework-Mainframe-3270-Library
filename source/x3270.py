be73770b06c9542afc556c480fa89824ccb63724
EUROPE\scabral <samuel.cabral@altran.com>
2019-02-28 16:55:35 +0000

    Try Catch to run in Pycharm, and make a documentation in libdoc with no error

--- source/x3270.py
+++ source/x3270.py
@@ -1,9 +1,9 @@
 # -*- encoding: utf-8 -*-
-from .py3270 import Emulator
 import time
 import os
 import socket
 import re
+from .py3270 import Emulator
 from robot.api import logger
 from robot.libraries.BuiltIn import BuiltIn
 from robot.utils import Matcher
@@ -21,7 +21,12 @@ class x3270(object):
         self.port = None
         self.credential = None
         self.imgfolder = img_folder
-        self.output_folder = BuiltIn().get_variable_value('${OUTPUT DIR}')
+        # if not self.plugin_folder : self.plugin_folder = os.getcwd() ## For PyCharm
+        try:
+            self.output_folder = BuiltIn().get_variable_value('${OUTPUT DIR}')
+        except Exception as e:
+            if hasattr(e, 'message') and e.message == 'Cannot access execution context': self.output_folder = os.getcwd()
+            else: raise AssertionError(e)
         self.wait = float(wait_time)
         self.timeout = int(timeout)
         self.mf = Emulator(visible=bool(visible), timeout=int(timeout))
