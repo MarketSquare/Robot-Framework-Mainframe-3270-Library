9d517f0c1eb69ea9e052acd6f39d970cc81f5d7c
Samuel Cabral <samuel.cabral@altran.com>
2021-02-05 16:20:41 +0000

    indentation

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open(version_file) as file:
     code = compile(file.read(), version_file, 'exec')
     exec(code)
 
-setup(name         		= 'robotframework-mainframe3270',
-      version      		= '2.10',
-      description  		= 'Mainframe Test library for Robot Framework',
-	  long_description	= 'Test library for Robot Framework to enable to create automated test scripts to test IBM Mainframe 3270',
-      author       		= 'Altran Portugal',
-      author_email 		= 'samuel.cabral@altran.com',
-	  license      		= 'MIT License',
-      url          		= 'https://github.com/Altran-PT-GDC/Robot-Framework-Mainframe-3270-Library',
-      packages  		= ['Mainframe3270'],
-      install_requires  = ['robotframework', 'six']
+setup(name             = 'robotframework-mainframe3270',
+      version          = '2.10',
+      description      = 'Mainframe Test library for Robot Framework',
+	  long_description = 'Test library for Robot Framework to enable to create automated test scripts to test IBM Mainframe 3270',
+      author           = 'Altran Portugal',
+      author_email     = 'samuel.cabral@altran.com',
+	  license          = 'MIT License',
+      url              = 'https://github.com/Altran-PT-GDC/Robot-Framework-Mainframe-3270-Library',
+      packages         = ['Mainframe3270'],
+      install_requires = ['robotframework', 'six']
       )
