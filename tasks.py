143c0345f3db6f1b75b3cb91a45c8702b1fe7b3a
Robin Matz <robin.s.matz@gmail.com>
2022-01-15 08:21:37 +0100

    format robot code with robotidy

--- tasks.py
+++ tasks.py
@@ -3,7 +3,23 @@ from invoke import task
 
 @task
 def lint_python(c):
-    """Perform code formatting with black, isort and flake8"""
+    """Perform python code formatting with black, isort and flake8"""
     c.run("black ./tasks.py Mainframe3270/")
     c.run("isort ./tasks.py Mainframe3270/")
     c.run("flake8 ./tasks.py Mainframe3270/")
+
+
+@task
+def lint_robot(c):
+    """Perform robot code formatting with robotidy"""
+    c.run("robotidy tests/")
+
+
+@task(lint_python, lint_robot)
+def lint(c):
+    """
+    Perform code formatting for both robot and python code.
+
+    Short option for `inv lint-python && inv lint-robot`.
+    """
+    pass
