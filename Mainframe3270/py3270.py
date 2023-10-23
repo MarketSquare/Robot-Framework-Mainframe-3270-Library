33e8a173027e58e8bc962197dbcd779b54584d6b
Robin Matz <robin.s.matz@gmail.com>
2023-10-23 21:02:27 +0200

    take advantage of re.IGNORECASE when search is done case-insensitively

--- Mainframe3270/py3270.py
+++ Mainframe3270/py3270.py
@@ -488,9 +488,8 @@ class Emulator(object):
     def get_string_positions(self, string, ignore_case=False):
         """Returns a list of tuples of ypos and xpos for the position where the `string` was found,
         or an empty list if it was not found."""
-        screen_content = self.read_all_screen().lower() if ignore_case else self.read_all_screen()
-        string = string.lower() if ignore_case else string
-        indices_object = re.finditer(re.escape(string), screen_content)
+        screen_content = self.read_all_screen()
+        indices_object = re.finditer(re.escape(string), screen_content, flags=0 if not ignore_case else re.IGNORECASE)
         indices = [index.start() for index in indices_object]
         # ypos and xpos should be returned 1-based
         return [self._get_ypos_and_xpos_from_index(index + 1) for index in indices]
