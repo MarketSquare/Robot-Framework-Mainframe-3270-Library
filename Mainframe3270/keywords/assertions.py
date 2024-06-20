2a5502bcd59c18bce01aa3a721727222d79310fb
wacharin tangseree <wacharin.t@doppiotech.com>
2024-06-20 16:59:49 +0700

    Unicode read / write support

--- Mainframe3270/keywords/assertions.py
+++ Mainframe3270/keywords/assertions.py
@@ -1,8 +1,10 @@
 import re
 from typing import List, Optional
+
 from robot.api import logger
 from robot.api.deco import keyword
 from robot.utils import Matcher
+
 from Mainframe3270.librarycomponent import LibraryComponent
 
 
